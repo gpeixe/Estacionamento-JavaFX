be6333dd719b199e6a61ee1ee3c1690da287b01d
yuridias0409 <62430812+yuridias0409@users.noreply.github.com>
2020-07-29 22:17:26 -0300

    TelaPrincipal e TelaInicial

--- src/Utils/Main.java
+++ src/Utils/Main.java
@@ -4,34 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    /*
-    @Override
-    public void start(Stage TicketStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../View/TelaTicket.fxml"));
-        TicketStage.setTitle("Gerar Ticket");
-        TicketStage.setResizable(false); //Importante para que o usuário não consiga redimensionar a tela
-        TicketStage.setScene(new Scene(root, 350, 391));
-        TicketStage.initModality(Modality.APPLICATION_MODAL);
-        TicketStage.showAndWait();
-    }
-    */
-
     @Override
     public void start(Stage LoginStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../View/TelaLogin.fxml"));
-        LoginStage.setTitle("Login");
-        LoginStage.setResizable(false); //Importante para que o usuário não consiga redimensionar a tela
-        LoginStage.setScene(new Scene(root, 350, 318));
-        //LoginStage.initModality(Modality.APPLICATION_MODAL);
-        //LoginStage.showAndWait();
+        Parent root = FXMLLoader.load(getClass().getResource("../View/TelaInicioSistema.fxml"));
+        LoginStage.setTitle("Estacionamento");
+        LoginStage.setResizable(false);
+        LoginStage.setScene(new Scene(root, 600, 346));
         LoginStage.show();
     }
-
     public static void main(String[] args) {
         launch(args);
     }
