335167665505c448c8d451b9efa0108258d15cd1
yuridias0409 <62430812+yuridias0409@users.noreply.github.com>
2020-07-30 12:31:08 -0300

    TelaCadUpdMensalista e TelaCadUpdFuncionario

---
+++ src/Controller/MensalistaController.java
@@ -0,0 +1,11 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+
+public class MensalistaController {
+    public void salvarMensalista(ActionEvent actionEvent) {
+    }
+
+    public void cancelaOp(ActionEvent actionEvent) {
+    }
+}
