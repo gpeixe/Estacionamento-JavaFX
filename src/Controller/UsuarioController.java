0aeb2d0c8c4329a0a4b0f9cce10d0c3b5d845bc1
yuridias0409 <62430812+yuridias0409@users.noreply.github.com>
2020-07-31 17:48:17 -0300

    Conexão Telas Admin

--- src/Controller/UsuarioController.java
+++ src/Controller/UsuarioController.java
@@ -1,6 +1,10 @@
 package Controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -8,13 +12,32 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class UsuarioController {
+    @FXML
+    JFXButton btnLogar;
+    @FXML
+    JFXPasswordField senhaUser;
+    @FXML
+    JFXTextField cpfUser;
+
     public void login(ActionEvent actionEvent) throws Exception{
-        Stage principalStage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("../View/TelaPrincipal.fxml"));
-        principalStage.setScene(new Scene(root, 618, 496));
-        principalStage.setResizable(false);
-        principalStage.setTitle("Login");
-        principalStage.initModality(Modality.WINDOW_MODAL);
-        principalStage.showAndWait();
+        if(cpfUser.getText().equals("admin")){
+            Stage principalStage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("../View/TelaAdmin.fxml"));
+            principalStage.setScene(new Scene(root, 910, 496));
+            principalStage.setResizable(false);
+            principalStage.setTitle("ADMIN");
+            principalStage.initModality(Modality.WINDOW_MODAL);
+            principalStage.showAndWait();
+        }   else if(cpfUser.getText().equals("vig")) {
+            /*Voltar pra onde estava, no caso fechar o modal*/
+        }   else if(cpfUser.getText().equals("atendente")) {
+            Stage principalStage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("../View/TelaPrincipal.fxml"));
+            principalStage.setScene(new Scene(root, 618, 496));
+            principalStage.setResizable(false);
+            principalStage.setTitle("Principal");
+            principalStage.initModality(Modality.WINDOW_MODAL);
+            principalStage.showAndWait();
+        }
     }
 }
