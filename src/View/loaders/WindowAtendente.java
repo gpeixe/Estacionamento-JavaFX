e00a2b1c72e2fac6fe11d401b657565fced60c1c
yuridias0409 <62430812+yuridias0409@users.noreply.github.com>
2020-08-29 17:27:42 -0300

    Organização Views + Icon

--- src/View/loaders/WindowAtendente.java
+++ src/View/loaders/WindowAtendente.java
@@ -6,6 +6,7 @@ import Model.Entities.Funcionarios.Atendente;
 import Model.Entities.Funcionarios.Funcionario;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -29,6 +30,8 @@ public class WindowAtendente {
             wa.setVagasTotais();
             wa.setLabelPrecos();
 
+            Image icon = new Image("Images/icon.png");
+            stage.getIcons().add(icon);
             stage.setScene(scene);
             stage.setTitle("Funcionário");
             stage.initModality(Modality.APPLICATION_MODAL);
--- src/View/loaders/WindowFuncionario.java
+++ src/View/loaders/WindowFuncionario.java
@@ -6,6 +6,7 @@ import Model.Entities.Funcionarios.Funcionario;
 import Model.Entities.Mensalista.Mensalista;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -26,7 +27,8 @@ public class WindowFuncionario {
                 fc.editFuncionario(funcionario);
                 fc.setFuncionario(funcionario);
             }
-
+            Image icon = new Image("Images/icon.png");
+            stage.getIcons().add(icon);
             stage.setScene(scene);
             stage.setTitle("Funcionário");
             stage.initModality(Modality.APPLICATION_MODAL);
